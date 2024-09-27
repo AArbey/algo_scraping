7426def303cdb997f2aadb373913ef8d46153714
Ni-co-las <47307853+Ni-co-las@users.noreply.github.com>
2024-09-27 13:14:01 +0200

    Update CDiscount scraper

---
+++ autorun_cdiscount_script.ps1
@@ -0,0 +1,4 @@
+while ($true) {
+& "C:/Users/nsoulie/AppData/Local/Microsoft/WindowsApps/python3.10.exe" "C:/Users/nsoulie/Documents/algo_scraping/cdiscount_scrap_test.py"
+Start-Sleep -Seconds 10800
+}
